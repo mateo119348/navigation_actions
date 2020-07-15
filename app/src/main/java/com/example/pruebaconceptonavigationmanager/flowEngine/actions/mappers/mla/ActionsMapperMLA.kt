fac72c1ba270b2f56c6ff4ccec2e6a53489d2dd7
mateo119348 <mateo.pedemonte@mercadolibre.com>
2020-07-15 17:17:23 -0300

    _Correcciones menores

--- app/src/main/java/com/example/pruebaconceptonavigationmanager/flowEngine/actions/mappers/mla/ActionsMapperMLA.kt
+++ app/src/main/java/com/example/pruebaconceptonavigationmanager/flowEngine/actions/mappers/mla/ActionsMapperMLA.kt
@@ -1,27 +1,27 @@
-package com.example.pruebaconceptonavigationmanager.flowEngine.actions.mappers
+package com.example.pruebaconceptonavigationmanager.flowEngine.actions.mappers.mla
 
 import com.example.navigation.flowEngine.actions.RuleAction
 import com.example.navigation.flowEngine.steps.field.FieldMapper
-import com.example.pruebaconceptonavigationmanager.activities.calculator.CalculatorActivity
+import com.example.pruebaconceptonavigationmanager.activities.calculator.mla.CalculatorActivityMLA
 import com.example.pruebaconceptonavigationmanager.activities.chooser.ChooserActivity
 import com.example.pruebaconceptonavigationmanager.activities.description.DescriptionActivity
 import com.example.pruebaconceptonavigationmanager.activities.installments.InstallmentsActivity
 import com.example.pruebaconceptonavigationmanager.activities.posSelector.PosSelectorActivity
 import com.example.pruebaconceptonavigationmanager.flowEngine.actions.ActionWrapper
+import com.example.pruebaconceptonavigationmanager.flowEngine.actions.mappers.ActionsMapperBase
 import com.example.pruebaconceptonavigationmanager.flowEngine.actions.Actions.CALCULATOR
 import com.example.pruebaconceptonavigationmanager.flowEngine.actions.Actions.CHOOSER
 import com.example.pruebaconceptonavigationmanager.flowEngine.actions.Actions.DESCRIPTION
 import com.example.pruebaconceptonavigationmanager.flowEngine.actions.Actions.INSTALLMENTS
 import com.example.pruebaconceptonavigationmanager.flowEngine.actions.Actions.POS_SELECTOR
 
-open class ActionsMapperBase {
+class ActionsMapperMLA : ActionsMapperBase() {
 
-    open fun getActionWrapper(ruleAction: RuleAction, fieldMapper: FieldMapper): ActionWrapper {
-
+    override fun getActionWrapper(ruleAction: RuleAction, fieldMapper: FieldMapper): ActionWrapper {
         val fields = fieldMapper.getFields(ruleAction.fields)
 
         return when (ruleAction.id) {
-            CALCULATOR.name -> ActionWrapper(CALCULATOR, fields, CalculatorActivity::class.java)
+            CALCULATOR.name -> ActionWrapper(CALCULATOR, fields, CalculatorActivityMLA::class.java)
             CHOOSER.name -> ActionWrapper(CHOOSER, fields, ChooserActivity::class.java)
             POS_SELECTOR.name -> ActionWrapper(POS_SELECTOR, fields, PosSelectorActivity::class.java)
             INSTALLMENTS.name -> ActionWrapper(INSTALLMENTS, fields, InstallmentsActivity::class.java)
--- navigation/src/main/java/com/example/navigation/flowEngine/steps/rules/comparable/GreaterThanRule.kt
+++ navigation/src/main/java/com/example/navigation/flowEngine/steps/rules/comparable/GreaterThanRule.kt
@@ -4,7 +4,7 @@ import com.example.navigation.flowEngine.steps.flowState.FlowState
 import java.math.BigDecimal
 
 class GreaterThanRule : ComparableRule() {
-    private lateinit var count: BigDecimal
+    lateinit var count: BigDecimal
 
     override fun evaluate(flowState: FlowState): Boolean {
         val value = getValue(flowState)
